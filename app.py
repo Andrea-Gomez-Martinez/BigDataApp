db30726ab1febd9bebcdd0ce7bbebe77c5427bd9
Andrea-Gomez-Martinez <agomezm17@ucentral.edu.co>
2025-06-04 21:15:57 -0500

    Update app.py
    Update app.py

--- app.py
+++ app.py
@@ -615,7 +615,6 @@ def elastic_eliminar_documento():
 @app.route('/buscador', methods=['GET', 'POST'])
 def buscador():
     
-   
     if request.method == 'POST':
         try:
             # Obtener los parámetros del formulario
@@ -692,33 +691,6 @@ def buscador():
             }
             query["query"]["bool"]["must"].append(range_query)
 
-                        # Capturar filtros seleccionados
-            categorias_seleccionadas      = request.form.getlist('categoria')
-            clasificaciones_seleccionadas = request.form.getlist('clasificacion')
-            anios_seleccionados           = request.form.getlist('Fecha')
-
-            if categorias_seleccionadas:
-                query["query"]["bool"]["must"].append({
-                    "terms": {
-                        "categoria": categorias_seleccionadas
-                    }
-                })
-
-            if clasificaciones_seleccionadas:
-                query["query"]["bool"]["must"].append({
-                    "terms": {
-                        "clasificacion": clasificaciones_seleccionadas
-                    }
-                })
-
-            if anios_seleccionados:
-                query["query"]["bool"]["must"].append({
-                    "terms": {
-                        "fecha": anios_seleccionados
-                    }
-                })
-
-
             # Ejecutar la búsqueda en Elasticsearch
             response = client.search(
                 index=INDEX_NAME,
@@ -749,6 +721,7 @@ def buscador():
     return render_template('buscador.html',
                         version=VERSION_APP,
                         creador=CREATOR_APP)
+
 
 @app.route('/api/search', methods=['POST'])
 def search():
